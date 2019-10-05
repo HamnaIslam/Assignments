c0a2245129446bf35cf832f5835217edb4b6c862
Hamna Islam <hamnaislam4848@gmail.com>
2019-10-05 15:07:38 +0500

    My 2nd Commit

---
+++ ex.py
@@ -0,0 +1,16 @@
+x=6
+print(x)
+y=x+7
+print(y)
+
+print('hello world')
+print('You must be legendary god that comes from the sky')
+print('We have been waiting for you for a long time')
+print('Our legends says you will be very tasty with mustard')
+print('We will have a feast tonight unless you say')
+
+x=43
+x=x+1
+print(x)
+print('I hate python')
+print('Good Bye')
