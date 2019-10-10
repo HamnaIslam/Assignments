c6be62b35583c0ad82c457bfcb320d0fde5bcfcc
Hamna Islam <hamnaislam4848@gmail.com>
2019-10-10 22:51:26 +0500

    My 3rd Commit

---
+++ ex2.py
@@ -0,0 +1,71 @@
+name = input("Enter your name\n")
+print ("Hello", name, "!")
+
+hours=input("Enter Hour:")
+rate=input("Enter Rate per Hour:")
+pay=float(hours)*float(rate)
+
+print("Pay:", pay)
+
+width = 17
+height = 12.0
+
+print(width/2)
+print(width/2)
+
+print (width/2.0)
+print(width/2.0)
+
+print (height/3)
+print(height/3)
+
+print (1 + 2 * 5)
+print(1 + 2 * 5)
+
+temp_Celcius = input('Enter a temperature in Celcius: ')
+temp_Farenheight = (float(temp_Celcius) * 1.8) + 32
+print ('This is equivalent to '),
+print (temp_Farenheight,)
+print (' degrees Farenheight')
+
+print(4)
+
+print(1,000,000)
+
+n = 17
+pi = 3.1416
+print(n)
+print(pi)
+
+print(1)
+x=2
+print(x)
+
+minute = 59
+print(minute/60)
+
+quotient = 7 // 3
+print(quotient)
+
+remainder = 7 % 3
+print(remainder)
+
+first = 10
+second = 15
+print(first+second)
+
+first = '100'
+second = '150'
+print(first + second)
+
+first = 'Test '
+second = 3
+print(first * second)
+
+
+
+
+
+
+
+
